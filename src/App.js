348cdf09809f93fb2ad3b1ceec02972692b8041b
Lewbagz <lewishardie@gmail.com>
2023-11-28 19:20:05 +1100

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+
+
+import './App.css';
+import PokemonHook from './PokemonHook';
+
+function App() {
+  return (
+    <div className="App">
+      <PokemonHook />
+      
+    </div>
+  );
+}
+
+export default App;
---
+++ src/PokemonHook.jsx
@@ -0,0 +1,8 @@
+
+export default function PokemonHook(){
+    return(
+        <div>
+            <h1>Feature not yet implemented</h1>
+        </div>
+    )
+}
