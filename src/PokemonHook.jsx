eb5f4ba4ee737fcfa4c4d7f3badafadcab3403e3
Lewbagz <lewishardie@gmail.com>
2023-11-28 20:46:36 +1100

    Get API, and useEffect options

--- src/PokemonHook.jsx
+++ src/PokemonHook.jsx
@@ -1,29 +1,73 @@
 
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 
 export default function PokemonHook(){
     //-- [variable of state, function to set variable] = useState(initial value of the state)
     let [pokemon, setPokemon] = useState({})
-    let [someExampleState, setSomeExampleState] = useState("hello world")
 
+    //-- Alternative to if structure in the return statement
+    // let [pokemon, setPokemon] = useState({name: "Loading... "})
+    
     //-- Don't do setState in top level of a functional component
     // setPokemon({name: "pikachu"});
 
+    //-- async operations and promises must be executed inside
+        //-- a new async block/scope within the useEffect callback
+        let fetchData = async () => {
+            let randomPokemonNumber = Math.floor(Math.random() * 1017) + 1;
+            let response = await fetch(`https://pokeapi.co/api/v2/pokemon/${randomPokemonNumber}`);
+            let data = await response.json();
+            // setPokemon({name: data.name})
+            setPokemon(data);
+        };
+
+
     function getPokemon(){
-        setPokemon({name: "pikachu"});
+        fetchData();
+    };
 
-    }
-
+    //-- useEffect(cb func) is equivalent to componentDidUpdate
     useEffect(() => {
-        setPokemon({name: "pikachu"})
-    },)
+        console.log("It triggers everytime a state changes, becauses there isnt a dependency array")
+    });
 
-    return(
-        <div>
-            <h1>{pokemon.name}</h1>
-            <h1>{someExampleState}</h1>
-            <button onClick={getPokemon}>Get the pokemon!</button>
-        </div>
-    )
-}
+    //-- runs at the start or on the first render of this componenet
+    //-- useEffect(cb func, []) is equivalent to componentDidMount
+    //-- Because the dependency array is empty
+    useEffect(() => {
+        fetchData();
+
+    }, []);
+
+    //-- this one triggers when there is a change in the pokemon state
+    useEffect(() => {
+        console.log("There was a change in the pokemon state")
+    }, [pokemon]);
+
+    //-- useEffect(cb func (return), [])
+    //-- The equivalent to componentDidUnmount
+    useEffect(() => {
+        return (() => {
+            console.log("It triggers when the component is unmounted")
+        });
+
+    }, []);
+
+    //-- html content with if, else
+    if (pokemon.name) {
+        return(
+            <div>
+                <h1>{pokemon.name}</h1>
+                <img src={pokemon.sprites.front_default} alt="API Pokemon"/>
+                <button onClick={getPokemon}>Get the pokemon!</button>
+            </div>
+        );
+    } else {
+        return(
+            <div>
+                <h1>No data to load...</h1>
+            </div>
+        );
+    };
+};
